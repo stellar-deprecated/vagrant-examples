dca5241e1093460fb6fd4eb9dc626d109acc26a1
Mat Schaffer <mat@schaffer.me>
2015-04-15 21:28:19 +0900

    Pull out sudo calls & add a version check before updating c libs

--- scripts/runtime_c_libs.sh
+++ scripts/runtime_c_libs.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install -qq python-software-properties
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main'
-sudo add-apt-repository -y 'ppa:ubuntu-toolchain-r/test'
-sudo apt-get update -qq
-sudo apt-get install -qq libstdc++6 libpq5 lldb-3.6
+if dpkg-query -W libstdc++ libpq5 lldb-3.6; then
+  exit 0
+fi
+
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
+apt-get update -qq
+apt-get install -qq python-software-properties
+add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main'
+add-apt-repository -y 'ppa:ubuntu-toolchain-r/test'
+apt-get update -qq
+apt-get install -qq libstdc++6 libpq5 lldb-3.6
